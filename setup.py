734c44db90a2e50f65eb2c5fdceeaa08a6ea5ebc
Hunter Senft-Grupp <huntcsg@gmail.com>
2019-02-24 20:18:33 -0500

    Cut Release: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.md', 'rb') as f:
 
 setup(
     name='airflow-docker',
-    version='0.1.2',
+    version='0.1.3',
     description='An opinionated implementation of exclusively '
                       'using airflow DockerOperators for all Operators',
     long_description=LONG_DESCRIPTION,
