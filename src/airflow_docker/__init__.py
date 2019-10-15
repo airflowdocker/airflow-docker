6635c786b840a136d0d48cc1b8f4c602c060e07a
Dan Cardin <ddcardin@gmail.com>
2019-10-15 12:18:29 -0400

    Cut Release: 0.4.0 → 0.4.1

--- src/airflow_docker/__init__.py
+++ src/airflow_docker/__init__.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 import airflow
 from airflow_docker.operator import (
