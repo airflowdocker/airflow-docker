69b2c93aa43f8ecb978360fcf8e273a8057cba84
Dan Cardin <ddcardin@gmail.com>
2019-10-16 13:22:29 -0400

    Cut Release: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "rb") as f:
 
 setup(
     name="airflow-docker",
-    version="0.4.2",
+    version="0.4.3",
     description="An opinionated implementation of exclusively "
     "using airflow DockerOperators for all Operators",
     long_description=LONG_DESCRIPTION,
