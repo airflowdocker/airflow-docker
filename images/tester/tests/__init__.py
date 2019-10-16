195d417e21aa9d9bb1be659b202733bc83b5a92b
Dan Cardin <ddcardin@gmail.com>
2019-10-16 16:25:28 -0400

    Move tester dag directory to the right location and test files recursively.

--- images/tester/tests/__init__.py
+++ images/tester/tests/__init__.py
@@ -1,7 +1,9 @@
 import os
 
-DAG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dags')
+DAG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "dags")
 DAG_FILES = [
-    f for f in os.listdir(DAG_PATH)
-    if f.endswith('.py')
+    os.path.join(*subdir.split(os.path.sep)[3:], file)
+    for subdir, dirs, files in os.walk(DAG_PATH)
+    for file in files
+    if file.endswith(".py")
 ]
