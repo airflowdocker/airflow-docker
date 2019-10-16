3972ebad153d8d94f52c3252dc30bda1fc23be15
Dan Cardin <ddcardin@gmail.com>
2019-10-16 16:25:28 -0400

    Move tester dag directory to the right location and test files recursively.

--- images/tester/tests/test_dag_consistency.py
+++ images/tester/tests/test_dag_consistency.py
@@ -2,15 +2,15 @@ import os
 
 import pytest
 
-from tests import DAG_PATH, DAG_FILES
+from tests import DAG_FILES, DAG_PATH
 
 
-@pytest.mark.parametrize('dag_file', DAG_FILES)
+@pytest.mark.parametrize("dag_file", DAG_FILES)
 def test_all_dags_in_dag_bag(dag_bag, dag_file):
     """Assert that all the dags for which there are files exist in the airflow dag bag.
     """
     dag_files = {
-        os.path.split(dag.full_filepath)[-1]
+        os.path.join(*dag.full_filepath.split(os.path.sep)[3:])
         for dag in dag_bag.dags.values()
     }
 
