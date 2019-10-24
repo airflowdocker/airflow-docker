dca141022548ded2b6cf22ee78c5adbadd85ef05
Dan Cardin <ddcardin@gmail.com>
2019-10-24 12:50:21 -0400

    Print dag load failures in tester tests.

--- images/tester/tests/conftest.py
+++ images/tester/tests/conftest.py
@@ -1,9 +1,37 @@
+import logging
+import os
+import sys
+
 import airflow
 import pytest
 
 from tests import DAG_PATH
 
 
-@pytest.fixture(scope='session')
+class Logger(logging.Logger):
+    def __get__(self, instance, owner):
+        self.instance = instance
+        return self
+
+    def exception(self, message, *args):
+        self.instance.dagbag_exceptions[args] = sys.exc_info()[1]
+        return super().exception(message, *args)
+
+
+class DagBag(airflow.models.DagBag):
+    _log = Logger(".")
+
+    dagbag_exceptions = {}
+
+
+@pytest.fixture(scope="session")
 def dag_bag():
-    return airflow.models.DagBag(DAG_PATH)
+    return DagBag(DAG_PATH)
+
+
+@pytest.fixture(scope="session")
+def dag_files(dag_bag):
+    return {
+        os.path.join(*dag.full_filepath.split(os.path.sep)[3:])
+        for dag in dag_bag.dags.values()
+    }
