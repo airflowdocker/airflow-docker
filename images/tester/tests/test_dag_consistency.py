ec228aad05ec1540f3de4ad7e09e37850faac9db
Dan Cardin <ddcardin@gmail.com>
2019-10-24 12:50:21 -0400

    Print dag load failures in tester tests.

--- images/tester/tests/test_dag_consistency.py
+++ images/tester/tests/test_dag_consistency.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 import pytest
 
@@ -6,12 +7,10 @@ from tests import DAG_FILES, DAG_PATH
 
 
 @pytest.mark.parametrize("dag_file", DAG_FILES)
-def test_all_dags_in_dag_bag(dag_bag, dag_file):
+def test_all_dags_in_dag_bag(dag_bag, dag_file, dag_files):
     """Assert that all the dags for which there are files exist in the airflow dag bag.
     """
-    dag_files = {
-        os.path.join(*dag.full_filepath.split(os.path.sep)[3:])
-        for dag in dag_bag.dags.values()
-    }
-
+    key = (os.path.join(DAG_PATH, dag_file),)
+    if key in dag_bag.dagbag_exceptions:
+        raise dag_bag.dagbag_exceptions[key]
     assert dag_file in dag_files
---
+++ release.rst
@@ -0,0 +1,7 @@
+RELEASE_TYPE: patch
+
+* Ensure dag tester test failures print out the reason why they failed.
+
+Authors:
+
+* Dan Cardin
